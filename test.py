a8626d4f0b2e4417bbf5093a6b3d61cb96b89a9f
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-26 20:45:14 +0100

    Trying to get a better accuracy.

--- test.py
+++ test.py
@@ -17,17 +17,25 @@ predict = "G3"
 X = np.array(data.drop([predict], 1)) #Features
 Y = np.array(data[predict]) 
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size= 0.1) #90 % of our data to train and the other 10% to test
+
+'''
+best = 0 
+
+for _ in range(30):
+
+	x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size= 0.1) #90 % of our data to train and the other 10% to test
 
 
-"""linear = linear_model.LinearRegression() #Define the model
+	linear = linear_model.LinearRegression() #Define the model
 
-linear.fit(x_train, y_train)
+	linear.fit(x_train, y_train)
 
-acc = linear.score(x_test, y_test)
+	acc = linear.score(x_test, y_test)
 
-with open("studentmodel.pickle", "wb") as f:
-	pickle.dump(linear, f)""" #thanks to pickle we don't to train our model for each running.
+	if acc > best:
+		best = acc
+		with open("studentmodel.pickle", "wb") as f:
+			pickle.dump(linear, f)'''
 
 pickle_in = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
