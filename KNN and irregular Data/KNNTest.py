2e2a152f6dbea02798fef879c011f95e5f3796d2
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-27 12:17:44 +0100

    Create KNNTest.py

---
+++ KNN and irregular Data/KNNTest.py
@@ -0,0 +1,11 @@
+from sklearn import preprocessing
+
+import sklearn 
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("car.data")
+print(data.head()) #To check if our data is loaded correctly 
