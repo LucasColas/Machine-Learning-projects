fed79136d9a4e0337d9f8c32abc7855c4a861368
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-27 12:24:00 +0100

    Update KNNTest.py

--- KNN and irregular Data/KNNTest.py
+++ KNN and irregular Data/KNNTest.py
@@ -9,3 +9,22 @@ from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
 print(data.head()) #To check if our data is loaded correctly 
+
+#Converting Data
+le = preprocessing.LabelEncoder()
+
+#Take a list and return it an array containing the values
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety)) #features
+y = list(cls) #labels
+
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
