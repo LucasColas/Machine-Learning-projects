055e784f657617fbb07d50018f5d99fb98805870
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-28 21:13:10 +0100

    Update SVM.py

--- Support Vector Machines (SVM)/SVM.py
+++ Support Vector Machines (SVM)/SVM.py
@@ -7,3 +7,10 @@ cancer = datasets.load_breast_cancer()
 print("Features: ", cancer.feature_names)
 print("Labels: ", cancer.target_names)
 
+x = cancer.data
+y = cancer.target
+
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+
+print(x_train, y_train)
