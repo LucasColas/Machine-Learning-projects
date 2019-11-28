3d6302daa8bba076d2708453344c2ad74a71d58a
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-28 19:03:22 +0100

    Update KNNTest.py

--- KNN and irregular Data/KNNTest.py
+++ KNN and irregular Data/KNNTest.py
@@ -1,5 +1,4 @@
 from sklearn import preprocessing
-
 import sklearn 
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,7 +7,7 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
-#print(data.head()) #To check if our data is loaded correctly 
+print(data.head()) #To check if our data is loaded correctly 
 
 #Converting Data
 le = preprocessing.LabelEncoder()
@@ -22,9 +21,24 @@ lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
 cls = le.fit_transform(list(data["class"]))
 
+predict = "class"
 
 X = list(zip(buying, maint, door, persons, lug_boot, safety)) #features
 y = list(cls) #labels
 
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors=9)
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+	print("predicted : ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+
+	
