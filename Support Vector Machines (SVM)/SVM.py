a2e2ab178b1bd91f8f7a5e724d498f09c0dd93ee
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-12-01 11:52:26 +0100

    Update SVM.py

--- Support Vector Machines (SVM)/SVM.py
+++ Support Vector Machines (SVM)/SVM.py
@@ -1,11 +1,12 @@
 import sklearn
 from sklearn import svm
 from sklearn import datasets
+from sklearn import metrics
 
 cancer = datasets.load_breast_cancer()
 
-print("Features: ", cancer.feature_names)
-print("Labels: ", cancer.target_names)
+#print("Features: ", cancer.feature_names)
+#print("Labels: ", cancer.target_names)
 
 x = cancer.data
 y = cancer.target
@@ -13,4 +14,11 @@ y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-print(x_train, y_train)
+#print(x_train, y_train)
+
+clf = svm.SVC()
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test) 
+
+acc = metrics.accuracy_score(y_test, y_pred)
